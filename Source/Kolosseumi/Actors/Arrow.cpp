19c10848d790aa86006ebd2b0e9d989e037bf4a6
kallerouvinen <kalle_rouvinen@hotmail.com>
2026-01-04 18:20:52 +0200

    Make arrows somewhat work

--- Source/Kolosseumi/Actors/Arrow.cpp
+++ Source/Kolosseumi/Actors/Arrow.cpp
@@ -1,19 +1,16 @@
 // Copyright 2026 Kalle Rouvinen. All Rights Reserved.
 
 #include "Kolosseumi/Actors/Arrow.h"
+#include "Kolosseumi/Pawns/Gladiator.h"
 #include "Components/BoxComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
 
 AArrow::AArrow()
 {
-	PrimaryActorTick.bCanEverTick = true;
-
-	// RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
-	// RootComponent = RootScene;
-
 	CollisionComp = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Collision"));
 	CollisionComp->InitBoxExtent(FVector(10.0f, 0.5f, 0.5f));
 	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
-	CollisionComp->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
+	CollisionComp->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnBeginOverlap);
 	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
 	CollisionComp->CanCharacterStepUpOn = ECB_No;
 	CollisionComp->bReturnMaterialOnMove = true;
@@ -26,51 +23,37 @@ AArrow::AArrow()
 		MeshComponent->SetStaticMesh(MeshFinder.Object);
 	}
 	MeshComponent->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
-	// MeshComponent->SetCollisionProfileName(TEXT("Projectile"));
 	MeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
-	MeshComponent->OnComponentHit.AddDynamic(this, &ThisClass::OnHit);
 	MeshComponent->SetupAttachment(RootComponent);
 
-	// ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
-	// ProjectileMovement->UpdatedComponent = RootComponent;
-	// ProjectileMovement->InitialSpeed = 100.f;
-	// ProjectileMovement->MaxSpeed = 100.f;
-	// ProjectileMovement->bRotationFollowsVelocity = true;
-	// ProjectileMovement->bShouldBounce = false;
-	// ProjectileMovement->ProjectileGravityScale = 0.f;
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
+	ProjectileMovement->UpdatedComponent = RootComponent;
+	ProjectileMovement->InitialSpeed = 1000.f;
+	ProjectileMovement->MaxSpeed = 1000.f;
+	ProjectileMovement->bIsHomingProjectile = true;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->ProjectileGravityScale = 0.f;
 }
 
 void AArrow::BeginPlay()
 {
 	Super::BeginPlay();
 
-	MeshComponent->IgnoreActorWhenMoving(SourceActor, true);
+	CollisionComp->IgnoreActorWhenMoving(SourceActor, true);
+	ProjectileMovement->HomingTargetComponent = TargetActor->GetRootComponent();
 }
 
-void AArrow::Tick(float DeltaTime)
+void AArrow::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	Super::Tick(DeltaTime);
-
-	if (TargetActor)
+	if (OtherActor && OtherActor == TargetActor)
 	{
-		FVector Direction = (TargetActor->GetActorLocation() - GetActorLocation()).GetSafeNormal();
-		FVector NewLocation = GetActorLocation() + Direction * 1000.0f * DeltaTime;
-		SetActorLocation(NewLocation);
+		if (AGladiator* TargetGladiator = Cast<AGladiator>(OtherActor))
+		{
+			// TODO: Replace this placeholder damage application with actual logic
+			float DamageAmount = 20.f;
+			TargetGladiator->SetHealth(TargetGladiator->GetHealth() - DamageAmount);
+		}
 
-		FRotator NewRotation = Direction.Rotation();
-		SetActorRotation(NewRotation);
-	}
-}
-
-// TODO: Handle collision with the target gladiator and apply damage
-// float DamageAmount = AttackDamage + FMath::RandRange(-20, 20);
-// TargetGladiator->SetHealth(TargetGladiator->GetHealth() - DamageAmount);
-
-void AArrow::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
-{
-	if (OtherActor == TargetActor)
-	{
-		// TODO: Apply damage to the target gladiator
 		Destroy();
 	}
 }
--- Source/Kolosseumi/Actors/Arrow.h
+++ Source/Kolosseumi/Actors/Arrow.h
@@ -21,25 +21,21 @@ protected:
 	virtual void BeginPlay() override;
 
 public:
-	virtual void Tick(float DeltaTime) override;
-
 	void SetTarget(AActor* Target) { TargetActor = Target; }
 	void SetSource(AActor* Source) { SourceActor = Source; }
 
 protected:
-	// UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	// TObjectPtr<USceneComponent> RootScene;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<UBoxComponent> CollisionComp;
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	TObjectPtr<UStaticMeshComponent> MeshComponent;
-	// UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-	// TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovement;
 
 	AActor* SourceActor;
 	AActor* TargetActor;
 
 private:
 	UFUNCTION()
-	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
