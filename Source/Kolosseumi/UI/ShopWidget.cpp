0d89a07ead3923dff32c483c63b26d202df9e835
kallerouvinen <kalle_rouvinen@hotmail.com>
2026-01-05 15:24:54 +0200

    Implement shop-mechanic :D

--- Source/Kolosseumi/UI/ShopWidget.cpp
+++ Source/Kolosseumi/UI/ShopWidget.cpp
@@ -4,44 +4,13 @@
 #include "Kolosseumi/Libraries/KolosseumiGameplayTags.h"
 #include "Kolosseumi/Messages/ReturnToMainUIMessage.h"
 #include "Components/Button.h"
-#include "Components/ListView.h"
 #include "GameFramework/GameplayMessageSubsystem.h"
 
 void UShopWidget::NativeOnInitialized()
 {
 	Super::NativeOnInitialized();
 
-	BuyButton->OnClicked.AddDynamic(this, &ThisClass::OnBuyButtonClicked);
 	BackButton->OnClicked.AddDynamic(this, &ThisClass::OnBackButtonClicked);
-	ItemListView->OnItemSelectionChanged().AddUObject(
-			this,
-			&ThisClass::OnItemSelected);
-
-	UGameplayMessageSubsystem& MessageSubsystem = UGameplayMessageSubsystem::Get(this);
-	MatchEndListenerHandle = MessageSubsystem.RegisterListener(
-			KolosseumiTags::Message_MatchEnd,
-			this,
-			&ThisClass::OnMatchEnd);
-}
-
-void UShopWidget::NativeConstruct()
-{
-	Super::NativeConstruct();
-
-	GenerateNewItemsForSale();
-	BuyButton->SetIsEnabled(false);
-}
-
-void UShopWidget::NativeDestruct()
-{
-	UGameplayMessageSubsystem& MessageSubsystem = UGameplayMessageSubsystem::Get(this);
-	MessageSubsystem.UnregisterListener(MatchEndListenerHandle);
-
-	Super::NativeDestruct();
-}
-
-void UShopWidget::OnBuyButtonClicked()
-{
 }
 
 void UShopWidget::OnBackButtonClicked()
@@ -50,45 +19,4 @@ void UShopWidget::OnBackButtonClicked()
 	MessageSubsystem.BroadcastMessage(
 			KolosseumiTags::Message_ReturnToMainUI,
 			FReturnToMainUIMessage());
-}
-
-void UShopWidget::OnItemSelected(UObject* SelectedItem)
-{
-	UE_LOG(LogTemp, Warning, TEXT("Item selected in ShopWidget"));
-
-	// TODO: Update item info when player selects item
-
-	bool bCanBuy = (SelectedItem != nullptr);
-	bool bHasFunds = true; // TODO: Check player funds
-	BuyButton->SetIsEnabled(bCanBuy && bHasFunds);
-}
-
-void UShopWidget::OnMatchEnd(FGameplayTag Channel, const FMatchEndMessage& Message)
-{
-	GenerateNewItemsForSale();
-}
-
-void UShopWidget::GenerateNewItemsForSale()
-{
-	// TODO: Get random items from data table?
-
-	// TArray<FItemData> NewItems;
-	// int32 NumNewItems = 3;
-
-	// for (int32 i = 0; i < NumNewItems; ++i)
-	// {
-	// 	FItemData NewItem;
-
-	// 	NewItems.Add(NewItem);
-	// }
-
-	// TArray<UItemDataObj*> ItemDataObjects;
-	// for (const FItemData& ItemData : NewItems)
-	// {
-	// 	UItemDataObj* DataObj = NewObject<UItemDataObj>(this);
-	// 	DataObj->Init(ItemData);
-	// 	ItemDataObjects.Add(DataObj);
-	// }
-
-	// ItemListView->SetListItems(ItemDataObjects);
 }
--- Source/Kolosseumi/UI/ShopWidget.h
+++ Source/Kolosseumi/UI/ShopWidget.h
@@ -3,13 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Kolosseumi/Messages/MatchEndMessage.h"
 #include "Blueprint/UserWidget.h"
-#include "GameFramework/GameplayMessageSubsystem.h"
 #include "ShopWidget.generated.h"
 
 class UButton;
-class UListView;
 
 /**
  *
@@ -21,27 +18,11 @@ class KOLOSSEUMI_API UShopWidget : public UUserWidget
 
 protected:
 	virtual void NativeOnInitialized() override;
-	virtual void NativeConstruct() override;
-	virtual void NativeDestruct() override;
 
-	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
-	TObjectPtr<UListView> ItemListView;
-	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
-	TObjectPtr<UButton> BuyButton;
 	UPROPERTY(BlueprintReadOnly, meta = (BindWidget))
 	TObjectPtr<UButton> BackButton;
 
 private:
 	UFUNCTION()
-	void OnBuyButtonClicked();
-	UFUNCTION()
 	void OnBackButtonClicked();
-	UFUNCTION()
-	void OnItemSelected(UObject* SelectedItem);
-
-	UFUNCTION()
-	void OnMatchEnd(FGameplayTag Channel, const FMatchEndMessage& Message);
-	FGameplayMessageListenerHandle MatchEndListenerHandle;
-
-	void GenerateNewItemsForSale();
 };
